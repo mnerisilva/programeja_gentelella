55e234e4a5fa27168357663d9670918ecda5318d
mnerisilva <marceloneri@gmail.com>
2023-02-10 13:05:19 -0300

    update

--- production/js/general_script.js
+++ production/js/general_script.js
@@ -2,24 +2,60 @@ const obj = [
     {
         categoria: 'CSS',
         trilha: {
-            nome: "Introdução ao Css",
+            nome: "Introdução ao Css 1",
             aulas:
             [
-                'Aula 1 Css',
-                'Aula 2 Css',
-                'Aula 3 Css'
+                'Introdução ao Css 1 - Aula 1',
+                'Introdução ao Css 1 - Aula 2',
+                'Introdução ao Css 1 - Aula 3'
             ]
         }
     },
     {
-        categoria: 'Html',
+        categoria: 'CSS',
         trilha: {
-            nome: "Introdução ao Html",
+            nome: "Introdução ao Css 2",
             aulas:
             [
-                'Aula 1 Html',
-                'Aula 2 Html',
-                'Aula 3 Html'
+                'Introdução ao Css 2 - Aula 1',
+                'Introdução ao Css 2 - Aula 2',
+                'Introdução ao Css 2 - Aula 3'
+            ]
+        }
+    },
+    {
+        categoria: 'CSS',
+        trilha: {
+            nome: "Introdução ao Css 3",
+            aulas:
+            [
+                'Introdução ao Css 3 - Aula 1',
+                'Introdução ao Css 3 - Aula 2',
+                'Introdução ao Css 3 - Aula 3'
+            ]
+        }
+    },
+    {
+        categoria: 'HTML',
+        trilha: {
+            nome: "Introdução ao Html 1",
+            aulas:
+            [
+                'Introdução ao Html 1 - Aula 1',
+                'Introdução ao Html 1 - Aula 2',
+                'Introdução ao Html 1 - Aula 3'
+            ]
+        }
+    },
+    {
+        categoria: 'HTML',
+        trilha: {
+            nome: "Introdução ao Html 2",
+            aulas:
+            [
+                'Introdução ao Html 2 - Aula 1',
+                'Introdução ao Html 2 - Aula 2',
+                'Introdução ao Html 2 - Aula 3'
             ]
         }
     }
@@ -27,20 +63,49 @@ const obj = [
 
 
 
-    const _h3MenuSection = document.querySelector('#sidebar-menu .menu_section h3');
-    const _linkTrilha = document.querySelector('#sidebar-menu .menu_section .side-menu a .trilha-title');
-    _h3MenuSection.textContent = obj[0].categoria;
-    _h3MenuSection.style.backgraoundColor = 'red !important';
-    _linkTrilha.textContent = obj[0].trilha.nome;
+  //  const _h3MenuSection = document.querySelector('#sidebar-menu .menu_section h3');
+   // const _linkTrilha = document.querySelector('#sidebar-menu .menu_section .side-menu a .trilha-title');
+  //  _h3MenuSection.textContent = obj[0].categoria;
+   // _h3MenuSection.style.backgraoundColor = 'red !important';
+    //_linkTrilha.textContent = obj[0].trilha.nome;
 
-    const _xxxx = document.querySelectorAll('#sidebar-menu .menu_section .side-menu li .child_menu li a');
-    console.log(_xxxx.length);
-    console.log(_xxxx[0]);
+    //const _xxxx = document.querySelectorAll('#sidebar-menu .menu_section .side-menu li .child_menu li a');
+    const _sidebarMenu = document.querySelector('#sidebar-menu');
+    //console.log(_xxxx.length);
+    //console.log(_xxxx[0]);
 
     let contador = 0;
+    let string = '';
+
     for(item of obj[contador].trilha.aulas){
-        console.log(item);
-        //console.log(obj[contador].trilha.aulas.length);
-        _xxxx[contador].textContent = item;
-        contador = contador + 1;
+
+        string = `
+                    <div class="menu_section">
+        `;
+    
+        string = string + `
+                    <h3>${obj[contador].categoria}</h3>
+        `;
+
+        string = string + `        
+            <ul class="nav side-menu">
+                <li><a><i class="fa fa-bug"></i> ${obj[contador].trilha.nome} <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+            `;
+                        obj[contador].trilha.aulas.forEach(function(aula){
+                            string = string + `
+                            <li><a href="">${aula}</a></li>
+                            `;
+                        });
+        string = string + `            
+                    </ul>
+                </li>  
+            </ul> 
+            `;
+    
+        string = string + `
+            </div>
+        `;
     }
+
+    _sidebarMenu.innerHTML = string;
